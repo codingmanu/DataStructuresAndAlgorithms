4c00347cf377a378b6bf8755a6dbefdb35f01d72
Manuel Gomez <atcodingmanu@gmail.com>
2020-05-14 16:57:18 -0400

    Added queue and Binary tree problem from FB interview

--- DataStructures.playground/Contents.swift
+++ DataStructures.playground/Contents.swift
@@ -1,12 +1,12 @@
 /*:
-# Data Structures and Algorithms
-#### Manuel Gomez @codingManu
-
-Hash Tables, Linked Lists, Stacks, Queues, Trees, Tries, Graphs, Vectors, Heaps.
-
-## 1. Hash Tables
----
-*/
+ # Data Structures and Algorithms
+ #### Manuel Gomez @codingManu
+ 
+ Hash Tables, Linked Lists, Stacks, Queues, Trees, Tries, Graphs, Vectors, Heaps.
+ 
+ ## 1. Hash Tables
+ ---
+ */
 import Foundation
 
 // Input data
@@ -91,11 +91,11 @@ for word in words {
 //: ---
 //:  Uncomment this block to print an example
 /*
-print("------------------- [ HASH TABLE EXAMPLE ] ------------------")
-exampleHashTable1.printDescription()
-exampleHashTable2.printDescription()
-print("-------------------------------------------------------------")
-*/
+ print("------------------- [ HASH TABLE EXAMPLE ] ------------------")
+ exampleHashTable1.printDescription()
+ exampleHashTable2.printDescription()
+ print("-------------------------------------------------------------")
+ */
 //: ---
 
 
@@ -104,8 +104,8 @@ print("-------------------------------------------------------------")
 
 
 /*:
-## 2. Linked Lists
----
+ ## 2. Linked Lists
+ ---
  */
 
 // Input data.
@@ -136,7 +136,7 @@ class LinkedList<T: Equatable> {
     }
     
     func insert(_ data: T, afterNode: LinkedListNode<T>) {
-
+        
         // If the node we're inserting after is the last one on the list, just add it as the nextNode.
         if afterNode.nextNode == nil {
             afterNode.nextNode = LinkedListNode<T>(data)
@@ -145,6 +145,16 @@ class LinkedList<T: Equatable> {
             let newNode = LinkedListNode<T>(data)
             newNode.nextNode = afterNode.nextNode
             afterNode.nextNode = newNode
+        }
+    }
+    
+    func removeLast() -> LinkedListNode<T>? {
+        guard let head = head else { return nil }
+        if head.nextNode == nil {
+            self.head = nil
+            return head
+        } else {
+            return head.removeLast()
         }
     }
     
@@ -168,7 +178,7 @@ class LinkedListNode<T: Equatable> {
     init(_ data: T) {
         self.data = data
     }
-
+    
     func append(data: T) {
         if nextNode == nil {
             nextNode = LinkedListNode(data)
@@ -193,6 +203,16 @@ class LinkedListNode<T: Equatable> {
             return nextNode!.traverse()
         } else {
             return self
+        }
+    }
+    
+    func removeLast() -> LinkedListNode<T>? {
+        guard let nextNode = nextNode else { return nil }
+        if nextNode.nextNode == nil {
+            self.nextNode = nil
+            return nextNode
+        } else {
+            return nextNode.removeLast()
         }
     }
     
@@ -223,20 +243,25 @@ for word in listComponents {
 //: ---
 //:  Uncomment this block to print an example
 /*
-print("------------------ [ LINKED LIST EXAMPLE ] ------------------")
-exampleLinkedList.printDescription()
-
-if let foundNode = exampleLinkedList.find(data: "ipsum") {
-    print("*** Found node ***")
-    foundNode.printDescription()
-    
-    print("*** Insert node after found one ***")
-    exampleLinkedList.insert("TEST", afterNode: foundNode)
-    
-    print("*** Print list again to check if noded was inserted correctly ***")
-    exampleLinkedList.printDescription()
-}
-print("-------------------------------------------------------------")
+ print("------------------ [ LINKED LIST EXAMPLE ] ------------------")
+ exampleLinkedList.printDescription()
+ 
+ if let foundNode = exampleLinkedList.find(data: "ipsum") {
+ print("*** Found node ***")
+ foundNode.printDescription()
+ 
+ print("*** Insert node after found one ***")
+ exampleLinkedList.insert("TEST", afterNode: foundNode)
+ 
+ print("*** Print list again to check if noded was inserted correctly ***")
+ exampleLinkedList.printDescription()
+ 
+ print("*** Find last ***")
+ if let lastNode = exampleLinkedList.lastNode() {
+ lastNode.printDescription()
+ }
+ }
+ print("-------------------------------------------------------------")
  */
 //: ---
 
@@ -375,7 +400,7 @@ class DoublyLinkedListNode<T: Equatable> {
         if previousNode != nil {
             print("Previous Node: \(Unmanaged.passUnretained(previousNode!).toOpaque())")
         }
-
+        
         if nextNode == nil {
             print("Next Node: nil --> END OF LIST")
             print("-------------------------------------------------------------")
@@ -422,7 +447,7 @@ for word in listComponents {
 // Input data.
 let stackData = [1, 2, 3, 4, 5]
 
-// Stack object implemented using a doubly linked list.
+// Stack object using a doubly linked list. LIFO implementation (LAST IN FIRST OUT)
 class Stack<T: Equatable> {
     
     private var elements = DoublyLinkedList<T>()
@@ -452,23 +477,202 @@ for element in stackData {
 //: ---
 //:  Uncomment this block to print an example
 /*
-print("---------------------- [ STACK EXAMPLE ] ---------------------")
-// Print complete stack
-exampleStack.printDescription()
+ print("---------------------- [ STACK EXAMPLE ] ---------------------")
+ // Print complete stack
+ exampleStack.printDescription()
+ 
+ // Pop a couple items from the stack. The last item on the stack gets popped first.
+ print("Popped item: \(String(describing: exampleStack.pop()))")
+ print("Popped item: \(String(describing: exampleStack.pop()))")
+ 
+ // Print again to see items popped from the end of the stack.
+ exampleStack.printDescription()
+ 
+ // Add a couple more items
+ exampleStack.push(25)
+ exampleStack.push(15)
+ 
+ // Print to see they're added to the end of the stack, not matter the order.
+ exampleStack.printDescription()
+ print("-------------------------------------------------------------")
+ */
+//: ---
 
-// Pop a couple items from the stack. The last item on the stack gets popped first.
-print("Popped item: \(String(describing: exampleStack.pop()))")
-print("Popped item: \(String(describing: exampleStack.pop()))")
+/*:
+ ## 6. Queues
+ ---
+ */
+
+// Input data.
+let queueData = [1, 2, 3, 4, 5]
+
+// Queue Object. FIFO implementation (FIRST IN, FIRST OUT)
+class Queue<T: Equatable> {
+    
+    private var elements = LinkedList<T>()
+    
+    func enqueue(_ element: T) {
+        let newNode = LinkedListNode(element)
+        guard let head = elements.head else {
+            elements.head = newNode
+            return
+        }
+        newNode.nextNode = head
+        elements.head = newNode
+    }
+    
+    @discardableResult func dequeue() -> LinkedListNode<T>? {
+        guard let head = elements.head else { return nil }
+        return head.removeLast()
+    }
+    
+    func printDescription() {
+        elements.printDescription()
+    }
+    
+}
+
+let exampleQueue = Queue<Int>()
+for element in queueData {
+    exampleQueue.enqueue(element)
+}
+
+//: ---
+//:  Uncomment this block to print an example
+/*
+print("---------------------- [ QUEUE EXAMPLE ] ---------------------")
+// Print complete queue
+exampleQueue.printDescription()
+
+// Dequeue a couple items from the queue. The first item that entered the queue gets removed first.
+print("Dequeued item: \(String(describing: exampleQueue.dequeue()!.data))")
+print("Dequeued item: \(String(describing: exampleQueue.dequeue()!.data))")
 
 // Print again to see items popped from the end of the stack.
-exampleStack.printDescription()
+exampleQueue.printDescription()
 
 // Add a couple more items
-exampleStack.push(25)
-exampleStack.push(15)
+exampleQueue.enqueue(15)
+exampleQueue.enqueue(30)
 
-// Print to see they're added to the end of the stack, not matter the order.
-exampleStack.printDescription()
+// Print to see they're always added to the back of the queue.
+exampleQueue.printDescription()
 print("-------------------------------------------------------------")
 */
 //: ---
+
+
+/*:
+ ## 7. Trees
+ ---
+ */
+/*:
+ #### a) Binary Tree example from FB Interview Prep
+ ---
+ */
+// Given the root node of a binary tree, return an array with the average values of each level.
+/*
+       4
+      /  \
+     7    9
+    / \    \
+   10  2    6
+        \
+         6
+        /
+       2
+ */
+
+func fillExampleBinaryTree() -> BinaryTreeNode<Int> {
+    let root = BinaryTreeNode(4)
+    root.leftNode = BinaryTreeNode(7)
+    root.rightNode = BinaryTreeNode(9)
+    root.leftNode?.leftNode = BinaryTreeNode(10)
+    root.leftNode?.rightNode = BinaryTreeNode(2)
+    root.rightNode?.rightNode = BinaryTreeNode(6)
+    root.leftNode?.rightNode?.rightNode = BinaryTreeNode(6)
+    root.leftNode?.rightNode?.rightNode?.leftNode = BinaryTreeNode(2)
+    return root
+}
+
+class BinaryTreeNode<T> {
+    
+    var data: T
+    var leftNode: BinaryTreeNode<T>?
+    var rightNode: BinaryTreeNode<T>?
+    
+    init(_ data: T) {
+        self.data = data
+    }
+}
+extension BinaryTreeNode: CustomStringConvertible where T == Int {
+    var description: String {
+        return "\(self.data)"
+    }
+}
+
+// Categorize nodes using Depth First Search.
+func categorizeWithArrays(node: BinaryTreeNode<Int>, data: inout [Int:[BinaryTreeNode<Int>]], depth: Int = 0) {
+    if data[depth] == nil {
+        data[depth] = [BinaryTreeNode<Int>]()
+    }
+    data[depth]?.append(node)
+    
+    if let leftNode = node.leftNode {
+        categorizeWithArrays(node: leftNode, data: &data, depth: depth + 1)
+    }
+    if let rightNode = node.rightNode {
+        categorizeWithArrays(node: rightNode, data: &data, depth: depth + 1)
+    }
+}
+
+func averagesForArrayData(data: inout [Int:[BinaryTreeNode<Int>]]) -> [Int] {
+    var result = [Int]()
+    
+    for level in data.sorted(by: { $0.key < $1.key }) {
+        let sum = level.value.reduce(into: 0) { (result, node) in
+            return result += node.data
+        }
+        result.append(sum / level.value.count)
+    }
+    return result
+}
+
+func categorizeWithTuples(node: BinaryTreeNode<Int>, data: inout [Int:(Int, Int)], depth: Int = 0) {
+    if data[depth] == nil {
+        data[depth] = (node.data,1)
+    } else {
+        data[depth]?.0 += node.data
+        data[depth]?.1 += 1
+    }
+    if let leftNode = node.leftNode {
+        categorizeWithTuples(node: leftNode, data: &data, depth: depth + 1)
+    }
+    if let rightNode = node.rightNode {
+        categorizeWithTuples(node: rightNode, data: &data, depth: depth + 1)
+    }
+}
+
+func averagesForTuplesData(data: inout [Int:(Int, Int)]) -> [Int] {
+    var result = [Int]()
+
+    for level in data.sorted(by: { $0.key < $1.key }) {
+        result.append(level.value.0 / level.value.1)
+    }
+    
+    return result
+}
+
+// Using arrays
+print("categorizeWithArrays() --> More space complexity: O(n) where n=number of elements in tree")
+var binaryTreeWithArraysData = [Int:[BinaryTreeNode<Int>]]() // Depth : [Elements]
+categorizeWithArrays(node: fillExampleBinaryTree(), data: &binaryTreeWithArraysData)
+print(binaryTreeWithArraysData.sorted(by: { $0.key < $1.key }))
+print(averagesForArrayData(data: &binaryTreeWithArraysData))
+
+// Using tuples
+print("categorizeWithTuples() --> Lesser space complexity: O(n) where n= number of levels in tree")
+var binaryTreeWithTuplesAverages = [Int:(Int, Int)]() // Depth : (Sum, Count))
+categorizeWithTuples(node: fillExampleBinaryTree(), data: &binaryTreeWithTuplesAverages)
+print(binaryTreeWithTuplesAverages.sorted(by: { $0.key < $1.key }))
+print(averagesForTuplesData(data: &binaryTreeWithTuplesAverages))
